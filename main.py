300851c3f0fc22d773f948b6b179bd74b15c2906
Vinicius D. Gubolin <vinicius.demite.gubolin@gmail.com>
2022-06-23 15:46:06 -0300

    feat: limitando número de piadas retornadas ao pesquisar

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import mimetypes
 from flask import Flask, Response, request, jsonify
 import requests
 
@@ -24,17 +25,30 @@ def category(name):
 @app.get(f'{BASE_URL}/filter')
 def filter():
   search = request.args.get('search')
-  limit = request.args.get('limit', default=10)
 
   if search == None:
     return Response(status=400, mimetype='application/json')
 
-  jokes = requests.get(f'https://api.chucknorris.io/jokes/search?query={search}&limit={limit}')
+  try:
+    limit = int(request.args.get('limit', default=10))
+  except ValueError:
+    return Response(response='O limite deve ser um número inteiro!!!', status=400, mimetype='application/json')
 
-  if jokes.status_code == 404:
+  response = requests.get(f'https://api.chucknorris.io/jokes/search?query={search}&limit={limit}')
+
+  if response.status_code == 404:
     return Response(status=404, mimetype='application/json')
 
-  return Response(response=jokes, status=200, mimetype='application/json')
+  jokes = response.json()
+  jokes_length = len(jokes['result'])
+
+  results = {
+    'limit': limit,
+    'results': jokes['result'][:limit] if limit <= jokes_length else jokes['result']
+  }
+
+  # return Response(response=results, status=200, mimetype='application/json')
+  return results
 
 
 if __name__ == "__main__":
